2917c6f339336bf6b075e95ca7090ae383bab820
i.karpov <karpovia@volgir.com>
2023-12-23 20:14:19 +0400

    complete stage 5

--- src/org/javaacadmey/wonder_field/Runner.java
+++ src/org/javaacadmey/wonder_field/Runner.java
@@ -1,7 +1,5 @@
 package org.javaacadmey.wonder_field;
 
-import org.javaacadmey.wonder_field.player.Player;
-
 public class Runner {
     public static void main(String[] args) {
         Game game = new Game();
