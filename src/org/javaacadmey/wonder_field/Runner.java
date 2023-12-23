597b3183aaece316e6e800bf822ea69f3e79c602
i.karpov <karpovia@volgir.com>
2023-12-23 23:55:31 +0400

    little update

--- src/org/javaacadmey/wonder_field/Runner.java
+++ src/org/javaacadmey/wonder_field/Runner.java
@@ -1,12 +1,9 @@
 package org.javaacadmey.wonder_field;
 
-import org.javaacadmey.wonder_field.wheel.Wheel;
-
 public class Runner {
     public static void main(String[] args) {
         Game game = new Game();
         game.init();
         game.startGame();
-//        Wheel wheel = new Wheel();
     }
 }
